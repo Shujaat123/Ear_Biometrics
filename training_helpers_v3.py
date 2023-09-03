6b4f6115c7b5f63265a6ece865c614689fff1ad3
5y3datif <63924971+5y3datif@users.noreply.github.com>
2023-09-03 21:03:45 +0500

    Update training_helpers_v3.py

--- training_helpers_v3.py
+++ training_helpers_v3.py
@@ -320,13 +320,13 @@ def train_folds(ear_images, sub_labels, k_folds, input_shape=(351, 246, 3),
   k_folds = max_state['kfolds']
   epochs_per_fold = max_state['epochs']
   
-  best_validation_accuracy = best_state['validation_accuracy']
-  best_validation_index = (best_state['trail']-1)*k_folds*epochs_per_fold + \
-    (best_state['fold']-1)*epochs_per_fold + (best_state['epoch']-1)
+  #best_validation_accuracy = best_state['validation_accuracy']
+  #best_validation_index = (best_state['trail']-1)*k_folds*epochs_per_fold + \
+  #  (best_state['fold']-1)*epochs_per_fold + (best_state['epoch']-1)
   
-  trail = current_state['trail']
-  fold = current_state['fold']
-  epoch = current_state['epoch']
+  #trail = current_state['trail']
+  #fold = current_state['fold']
+  #epoch = current_state['epoch']
   
   #resume
   if not resume_from == None:
@@ -372,9 +372,8 @@ def train_folds(ear_images, sub_labels, k_folds, input_shape=(351, 246, 3),
 
     # Reset model weights before each fold
     model.apply(reset_weights)
-    best_validation_accuracy = train_epochs(X_train, y_train, X_test, y_test, 
-                                            input_shape=input_shape, 
-                                            num_classes=num_classes, 
+    best_state = train_epochs(X_train, y_train, X_test, y_test, 
+                              input_shape=input_shape, num_classes=num_classes, 
                                             num_filters=num_filters, 
                                             model_type=model_type, 
                                             model=model, optimizer=optimizer, 
