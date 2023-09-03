3a64c7bafc8f5648fa43d920447fd5a23bdcffcb
5y3datif <63924971+5y3datif@users.noreply.github.com>
2023-09-03 10:41:05 +0500

    Update training_helpers_v3.py

--- training_helpers_v3.py
+++ training_helpers_v3.py
@@ -217,12 +217,12 @@ def train_epochs(X_train, y_train, X_test, y_test, input_shape=(351, 246, 3),
                                  'valid_loss': valid_loss/len(validation_loader), 
                                  'validation_accuracy': validation_accuracy}
     elif trail== 0:
-      results[fold-1][epoch-1]={'train_loss': train_loss/len(training_loader), 
+      results[(fold-1)*epochs+(epoch-1)]={'train_loss': train_loss/len(training_loader), 
                                  'training_accuracy': training_accuracy, 
                                  'valid_loss': valid_loss/len(validation_loader), 
                                  'validation_accuracy': validation_accuracy}
     else:
-      results[trail-1][fold-1][epoch-1]={'train_loss': train_loss/len(training_loader), 
+      results[(trail-1)*kfolds*epochs + (fold-1)*epochs + (epoch-1)]={'train_loss': train_loss/len(training_loader), 
                                  'training_accuracy': training_accuracy, 
                                  'valid_loss': valid_loss/len(validation_loader), 
                                  'validation_accuracy': validation_accuracy}
