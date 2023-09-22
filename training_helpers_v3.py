1afc416466f3312cd7b45bc6869b3504e3b11c94
5y3datif <63924971+5y3datif@users.noreply.github.com>
2023-09-22 06:44:08 +0500

    Update training_helpers_v3.py

--- training_helpers_v3.py
+++ training_helpers_v3.py
@@ -330,7 +330,6 @@ def reset_weights(m):
     Try resetting model weights to avoid
     weight leakage.
   '''
-  print('Reset trainable parameters of layers')
   for layer in m.children():
    if hasattr(layer, 'reset_parameters'):
     #print(f'Reset trainable parameters of layer = {layer}')
