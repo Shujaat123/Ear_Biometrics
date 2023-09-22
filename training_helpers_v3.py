1c228c868734f0542cd96d9f8ccc78565f64a9ed
5y3datif <63924971+5y3datif@users.noreply.github.com>
2023-09-22 06:51:06 +0500

    Update training_helpers_v3.py

--- training_helpers_v3.py
+++ training_helpers_v3.py
@@ -209,7 +209,7 @@ def train_epochs(X_train, y_train, X_test, y_test,
 
   
   fold_best_validation_accuracy = 0
-  fold_best_validation_accuracy_age = 0
+  fold_best_validation_accuracy_age = early_stop_thresh
   fold_best_validation_accuracy_epoch = 0
 
   #data
@@ -281,10 +281,12 @@ def train_epochs(X_train, y_train, X_test, y_test,
 
     #torch.save(latest_checkpoint, "latest_checkpoint.pth")
     
-    if validation_accuracy > fold_best_validation_accuracy:
+    if validation_accuracy >= fold_best_validation_accuracy:
         fold_best_validation_accuracy = validation_accuracy
-        fold_best_validation_accuracy_age = fold_best_validation_accuracy_age+1
+        fold_best_validation_accuracy_age = 0
         print(f"fold_best_validation_accuracy {fold_best_validation_accuracy}")
+    else
+        fold_best_validation_accuracy_age = fold_best_validation_accuracy_age+1
       
     if fold_best_validation_accuracy_age > early_stop_thresh:
         print(f"early_stop_thresh {early_stop_thresh} {fold_best_validation_accuracy_age}")
