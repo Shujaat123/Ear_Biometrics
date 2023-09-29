976bbe6792092fa0fe51ef4d088a4eae4397f069
5y3datif <63924971+5y3datif@users.noreply.github.com>
2023-09-29 08:29:23 +0500

    Update training_helpers_v3.py

--- training_helpers_v3.py
+++ training_helpers_v3.py
@@ -275,8 +275,8 @@ def train_epochs(X_train, y_train, X_test, y_test,
     print(f"\n Average Training Encoder Loss: {training_encoder_loss/len(training_loader)}")
     print(f"\n Average Training Classifier Loss: {training_classifier_loss/len(training_loader)}")
     print(f"Validation: \n Validation Accuracy: {validation_accuracy}%, Average Validation Loss: {validation_loss/len(validation_loader)}")
-    print(f"\n Average Validation Encoder Loss: {validation_encoder_loss/len(training_loader)}")
-    print(f"\n Average Validation Classifier Loss: {validation_classifier_loss/len(training_loader)}")
+    print(f"\n Average Validation Encoder Loss: {validation_encoder_loss/len(validation_loader)}")
+    print(f"\n Average Validation Classifier Loss: {validation_classifier_loss/len(validation_loader)}")
       
     if validation_accuracy > best_validation_accuracy: 
         best_validation_accuracy = validation_accuracy 
